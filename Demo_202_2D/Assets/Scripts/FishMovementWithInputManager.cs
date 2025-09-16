d9a328e2bc9cfb6446435cca0a22a6ae35759d09
Erin Cascioli <edcigm@rit.edu>
2025-09-16 11:33:50 -0400

    Pushing 2D project
    Initial push of the 2D project.

---
+++ Demo_202_2D/Assets/Scripts/FishMovementWithInputManager.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public class FishMovementWithInputManager : MonoBehaviour
+{
+
+    /// <summary>
+    /// Object that this script moves with WASD or arrow keys
+    /// </summary>
+    public GameObject fish;
+
+    /// <summary>
+    /// Unit-based movement per frame for the fish
+    /// </summary>
+    public float fishSpeed;
+
+    // ------------------------------------------------------------------------
+    // Rotate the fish every frame!  
+    // how much to rotate - no more than 1 degree per frame
+    // cumulative rotation
+    public float speedOfRotation;
+    public float totalRotation;
+
+    // ------------------------------------------------------------------------
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        // Start by moving fish independently on X or Y axis using per-frame movement.
+        //MoveFish();
+
+        // Rotate the fish
+        RotateFish();
+    }
+
+    public void RotateFish()
+    {
+        // Increase the total rotation by the rotation speed
+        totalRotation += speedOfRotation;
+
+        Debug.Log(totalRotation);
+
+        // Update the transform's rotation with a quaternion calculated by Unity
+        Quaternion newRotation = Quaternion.Euler(0, 0, totalRotation);
+        fish.transform.rotation = newRotation;
+    }
+
+
+    /// <summary>
+    /// Use Unity's Input Manager system to move the fish in 4 cardinal directions
+    ///    with WASD.
+    /// </summary>
+    public void MoveFish()
+    {
+        // Grab the fish's current position as a locally-declared struct
+        // such that it is modifiable.
+        Vector3 fishPosition = fish.transform.position;
+
+        // Move the jellyfish!
+        // When the A key is pressed, move fish to the left
+        if (Input.GetKey(KeyCode.A))				// Left
+        {
+            // Prove to ourselves that the key presses are working!
+            UnityEngine.Debug.Log("Pressing A");
+
+            // Move the object a tiny unit left
+            fishPosition.x -= fishSpeed;
+        }
+        //else if ()                       // Right
+        {
+            // How to move the fish? 
+        }
+        //else if ()                       // Up
+        {
+            // How to move the fish?
+        }
+        //else if ()                       // Down
+        {
+            // How to move the fish?
+        }
+
+        // Transport the fish to that position
+        fish.transform.position = fishPosition;
+    }
+
+
+}
